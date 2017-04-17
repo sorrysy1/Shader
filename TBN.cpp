b7693d29d2cac07cecec5a1faae25beb1226b9dd
sorrysy1 <sorrysy7@163.com>
2017-04-17 18:33:32 +0800

    Create TBN.cpp

---
+++ TBN.cpp
@@ -0,0 +1,96 @@
+//切线空间
+//T:tangent 切线
+//N:normal  法线
+//B:bitangent 副切线
+//切线空间用来计算点光照效果
+
+//vert
+#version 330 core
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 normal;
+layout (location = 2) in vec2 texCoords;
+layout (location = 3) in vec3 tangent;
+layout (location = 4) in vec3 bitangent;
+
+out VS_OUT {
+    vec3 FragPos;
+    vec2 TexCoords;
+    vec3 TangentLightPos;
+    vec3 TangentViewPos;
+    vec3 TangentFragPos;
+} vs_out;
+
+uniform mat4 projection;
+uniform mat4 view;
+uniform mat4 model;
+
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+void main()
+{
+    gl_Position = projection * view * model * vec4(position, 1.0f);
+    vs_out.FragPos = vec3(model * vec4(position, 1.0));   
+    vs_out.TexCoords = texCoords;
+    
+    mat3 normalMatrix = transpose(inverse(mat3(model)));
+    vec3 T = normalize(normalMatrix * tangent);
+    vec3 B = normalize(normalMatrix * bitangent);
+    vec3 N = normalize(normalMatrix * normal);    
+    mat3 TBN = transpose(mat3(T, B, N));  
+    /*
+    *当网格过多的情况下 会导致切线空间相互不垂直 与下列方法（格拉姆-施密特正交化）校正空间向量
+    *vec3 T = normalize(vec3(model * vec4(tangent, 0.0)));  
+    *vec3 N = normalize(vec3(model * vec4(tangent, 0.0)));  
+    *// re-orthogonalize T with respect to N  
+    *T = normalize(T - dot(T, N) * N);  
+    *// then retrieve perpendicular vector B with the cross product of T and N  
+    *vec3 B = cross(T, N);  
+    *mat3 TBN = mat3(T, B, N)  
+    */ 
+  
+    vs_out.TangentLightPos = TBN * lightPos;
+    vs_out.TangentViewPos  = TBN * viewPos;
+    vs_out.TangentFragPos  = TBN * vs_out.FragPos;
+}
+//frag
+#version 330 core
+out vec4 FragColor;
+
+in VS_OUT {
+    vec3 FragPos;
+    vec2 TexCoords;
+    vec3 TangentLightPos;
+    vec3 TangentViewPos;
+    vec3 TangentFragPos;
+} fs_in;
+
+uniform sampler2D diffuseMap;
+uniform sampler2D normalMap;
+
+uniform bool normalMapping;
+
+void main()
+{           
+    // Obtain normal from normal map in range [0,1]
+    vec3 normal = texture(normalMap, fs_in.TexCoords).rgb;
+    // Transform normal vector to range [-1,1]
+    normal = normalize(normal * 2.0 - 1.0);  // this normal is in tangent space
+
+    // Get diffuse color
+    vec3 color = texture(diffuseMap, fs_in.TexCoords).rgb;
+    // Ambient
+    vec3 ambient = 0.1 * color;
+    // Diffuse
+    vec3 lightDir = normalize(fs_in.TangentLightPos - fs_in.TangentFragPos);
+    float diff = max(dot(lightDir, normal), 0.0);
+    vec3 diffuse = diff * color;
+    // Specular
+    vec3 viewDir = normalize(fs_in.TangentViewPos - fs_in.TangentFragPos);
+    vec3 reflectDir = reflect(-lightDir, normal);
+    vec3 halfwayDir = normalize(lightDir + viewDir);  
+    float spec = pow(max(dot(normal, halfwayDir), 0.0), 32.0);
+    vec3 specular = vec3(0.2) * spec;
+    
+    FragColor = vec4(ambient + diffuse + specular, 1.0f);
+}
